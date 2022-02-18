cabef171ba3ca4def6b7e3f310b5d1e7db523713
shayan <shayanpardis82@gmail.com>
2022-02-19 02:51:46 +0330

    add event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import { PersonEdit, PersonCreate, PersonList } from "./person";
+import { EventList, EventTitle, EventCreate, EventEdit } from "./event";
 import Dashboard from './dashboard'
 import AuthProvider from './authProvider'
-import CollectionsIcon from '@material-ui/icons/Collections';
+import PersonIcon from '@material-ui/icons/Person';
 import EventIcon from '@material-ui/icons/Event';
 import DataProvider from './dataProvider'
 
 const App = () => <Admin dashboard={Dashboard} authProvider={AuthProvider} dataProvider={DataProvider}>
-  <Resource name="person" list={PersonList} icon={CollectionsIcon} edit={PersonEdit} create={PersonCreate} />
+  <Resource name="person" list={PersonList} icon={PersonIcon} edit={PersonEdit} create={PersonCreate} />
+  {/* <Resource name="event" list={PersonList} icon={CollectionsIcon} edit={PersonEdit} create={PersonCreate} /> */}
+  <Resource name="event" list={EventList} icon={EventIcon} edit={EventEdit} create={EventCreate} />
 </Admin>;
 
 export default App;
--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,9 +1,9 @@
 import { fetchUtils } from 'react-admin';
 
-const apiUrl = 'http://localhost:8888/api/v1';
+const apiUrl = `${window.location.origin}/api/v1`;
 const httpClient = fetchUtils.fetchJson;
 
-export default {
+const dataProvider = {
     getList: (resource) => {
         const url = `${apiUrl}/${resource}/`;
 
@@ -13,8 +13,17 @@ export default {
         }));
     },
 
-    getOne: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
+    getOne: (resource, params) => {
+        if(resource === "event") {
+            resource = "event/speech";
+        }
+        return httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
+            data: json,
+        }))
+    },
+
+    getMany: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/`).then(({ json }) => ({
             data: json,
         })),
 
@@ -24,16 +33,28 @@ export default {
             body: JSON.stringify(params.data),
         }).then(({ json }) => ({ data: json })),
 
-    create: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}/`, {
-            method: 'POST',
-            body: JSON.stringify(params.data),
-        }).then(({ json }) => ({
-            data: { ...params.data, id: json.id },
-        })),
+    create: (resource, params) => {
+        if (resource === "event") {
+            return httpClient(`${apiUrl}/${resource}/day/${params.data.date.replaceAll('-', '/')}/`, {
+                method: 'POST',
+                body: JSON.stringify(params.data),
+            }).then(({ json }) => ({
+                data: { ...params.data, id: json.id },
+            }))
+        } else {
+            return httpClient(`${apiUrl}/${resource}/`, {
+                method: 'POST',
+                body: JSON.stringify(params.data),
+            }).then(({ json }) => ({
+                data: { ...params.data, id: json.id },
+            }))
+        }
+    },
 
     delete: (resource, params) =>
         httpClient(`${apiUrl}/${resource}/${params.id}`, {
             method: 'DELETE',
         }).then(({ json }) => ({ data: json })),
 };
+
+export default dataProvider;
